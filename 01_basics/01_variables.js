98939ca3d587357c6c990ed3750cd88f840be01d
tirthvyas24 <tirth20052005@gmail.com>
2025-08-20 12:40:40 +0530

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accNo = 112244774455;
+let accEmail = "tirth@yahoo.co.in";
+var accPassword = "123tir345";
+accCity = "Ahmedabad";
+let accState;
+
+// accNo = 14796535;  // not allowed
+
+accEmail = "kamtir@gmail.com"
+accPassword = "1724";
+accCity = "Surat";
+
+console.log(accNo);
+console.table([accNo, accEmail, accPassword, accCity, accState]);
+/* prefer not to use var becoz of issue with block and functional scope */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version of JS
+
+// alert(5+5); // we are using node.js, not browser
+
+console.log(5
+    +
+    5 )  // code readability should be high
+
+console.log("Tirth");
+
+let num = 17;
+
+// number => 2 to the power 53
+// boolean => true/false
+// string => " "
+// null => standalone value  // temperature example: if server, due to some error, doesn't have the temperature value then it may send 0, but that is also a temperature value so null is used in this case
+// bigint
+// undefined => value hasn't been assigned yet
+// symbol => unique
+
+// above all are primitive datatype
+
+// object
+
+console.log(typeof "tirth");
+console.log(typeof null);  // returns "object"
+console.log(typeof undefined);  // returns "undefined"
+
+
+
+
+
