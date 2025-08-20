ed19a338e53fad23b24f6b899bbdc85e990dbeb0
tirthvyas24 <tirth20052005@gmail.com>
2025-08-20 14:05:30 +0530

    conversionOperations

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,49 @@
+let score = "123ab";
+let score1 = null;
+let score2 = undefined;
+let score3 = true
+
+// let scoreInNumber = Number(score)
+// console.log(typeof scoreInNumber);   // will return number
+// console.log(scoreInNumber);     // will return Nan value, basically js tries to convert the value into number datatype
+
+// let score1InNumber = Number(score1);
+// console.log(score1InNumber);  // this will return 0
+// console.log(typeof score1InNumber);  // this will return number
+
+// let score2InNumber = Number(score2);  
+// console.log(score2InNumber);  // will return Nan value
+// console.log(typeof score2InNumber);  // will return number
+
+// let score3InNumber = Number(score3); 
+// console.log(score3InNumber);   // will return 1
+// console.log(typeof score3InNumber);   // will return number
+
+
+let isLoggedIn = "nsxq";  // will return false when empty string otherwise true 
+let boolIsLogIn = Boolean(isLoggedIn);
+
+console.log(boolIsLogIn);  
+console.log(typeof boolIsLogIn);
+
+
+let someNumber = 45;
+let strNum = String(someNumber);
+
+console.log(strNum);
+console.log(typeof strNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
