8a24da86a34dd72a207e9b9c999f7cb939f32284
tirthvyas24 <tirth20052005@gmail.com>
2025-08-22 16:51:32 +0530

    Maths and numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,52 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));  // 100.00
+
+// const anotherNum = 123.49875;
+// console.log(anotherNum.toPrecision(3));   // returns string 
+
+// const anotherNum1 = 23.49875;
+// console.log(anotherNum1.toPrecision(3));   // returns string 
+
+// const anotherNum2 = 1178.49875;
+// console.log(anotherNum2.toPrecision(3));   // returns string 
+
+
+// const hundereds = 1000000;
+// console.log(hundereds.toLocaleString());   
+// console.log(hundereds.toLocaleString("en-IN"));  // put commas according to indian values 
+
+
+// ++++++++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-17));  // -ve to +ve
+// console.log(Math.round(24.5));
+// console.log(Math.floor(5.8));  // 5
+// console.log(Math.ceil(4.2));  // 5
+// console.log(Math.min(2,3,5,1));
+// console.log(Math.max(2,3,5,1));
+
+// console.log(Math.random());  // returns values bw 0 and 1
+// console.log((Math.random()*10) + 1);  // +1 is used to avoid 0
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min );
+
+
+
+
+
+
+
+
+
+
+
